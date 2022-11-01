a614b456d7a489009c20b6662296af47d8e9da3a
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-02 02:48:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = new express()
 const fs = require("fs")
 const ytdl = require("ytdl-core")
 let description = "You need to provide an id for us to convert, like this! <br> http://localhost:3000/?id=watch?v=IUPYpZBfsMU <br> Jeff Geerling is the creator of the video and is a wonderful contributor to the RPI, open source and some other communities! <br> go check his content out!"
+app.use(function(req,res){
+    res.status(404).send("🍌, 404")
+});
 
 app.get('/mp4', async (req, res) => {
 	if(!req.query.id) return res.send(description)
@@ -26,3 +29,4 @@ app.get('/mp4', async (req, res) => {
 	}
 })
 app.listen(3000)
+console.log("The app.listen() has been executed!")
