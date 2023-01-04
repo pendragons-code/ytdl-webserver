a49b7dccbdadec258b046118ed1109e4ac1df4f5
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2023-01-04 20:16:10 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require("express")
-const port = 3000
+const port = 8000
 const app = new express()
 const fs = require("fs")
 const ytdl = require("ytdl-core")
 const path = require("path")
 const ffmpeg = require("fluent-ffmpeg")
-let description = "You need to provide an id for us to convert, like this! <br> http://localhost:3000/mp4?id=IUPYpZBfsMU <br> Jeff Geerling is the creator of the video and is a wonderful contributor to the RPI, open source and some other communities! <br> go check his content out!"
+const config = require("./config.json")
+let description = `You need to provide an id for us to convert, like this! <br> http://${config.dns}:3000/mp4?id=IUPYpZBfsMU <br> Jeff Geerling is the creator of the video and is a wonderful contributor to the RPI, open source and some other communities! <br> go check his content out!`
 
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine','ejs')
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("index");
 });
 app.get('/mp4', async (req, res) => {
 	if(!req.query.id) return res.send(description)
-	try{	
+	try{
+
 		if(fs.existsSync(`./Videos/${req.query.id}.mp4`)) return res.send("You already have this downloaded in the server, delete this and download again!")
-		ytdl(`https://www.youtube.com/watch?v=${req.query.id}`, { quality: 'highestvideo' }).pipe(fs.createWriteStream(`./Videos/${req.query.id}.mp4`))
+		ytdl(`https://www.youtube.com/watch?v=${req.query.id}`, { filter: "audioandvideo", quality: "highest" }).pipe(fs.createWriteStream(`./Videos/${req.query.id}.mp4`))
 		res.write("Downloading!")
 		async function check(){
 			if(fs.existsSync(`./Videos/${req.query.id}.mp4`)){
-				res.write("\nDone")
-				res.end()
+				res.send("\nDone")
 				return
 			}else{
 				res.write("\nStill downloading, next check in 5 secs!")
@@ -36,7 +37,7 @@ app.get('/mp4', async (req, res) => {
 
 app.get('/mp3', async (req, res) => {
 	try{
-		let mp3desc = "To download an MP3, you need to provide the id of the video.<br> http://localhost:3000/mp3?id=IUPYpZBfsMU <br>"
+		let mp3desc = `To download an MP3, you need to provide the id of the video.<br> http://${config.dns}:3000/mp3?id=IUPYpZBfsMU <br>`
 		if(!req.query.id) return res.send(mp3desc)
 		if(fs.existsSync(`./Audios/${req.query.id}.mp3`)) return res.send("You have already downloaded this before, delete this from the server first to download again!")
 		let stream = ytdl(req.query.id, {
@@ -47,8 +48,7 @@ app.get('/mp3', async (req, res) => {
 		.audioBitrate(128)
 		.save(`./Audios/${req.query.id}.mp3`)
 		.on('end', () => {
-			res.write("Done!")
-			res.end()
+			res.send("Done!")
 		})
 	}catch(e){
 		console.log(e)
@@ -57,30 +57,72 @@ app.get('/mp3', async (req, res) => {
 })
 
 app.get('/list', async (req, res) => {
-	const directoryPath = path.join(__dirname, "Videos")
-	fs.readdir(directoryPath, function (err, files){
-		if(err) return res.send("Could not scan directory.\n\n" + err)
+	fs.readdir("./Videos", function (err, files){
+		if(err) res.write("Could not scan directory.\n\n" + err)
 		if(files.length === 0) res.write("Directory Videos empty!\n")
 		console.log("Videos:\n")
 		files.forEach(function (file){
 			res.write(`${file}\n`)
+			console.log(`${file}\n`)
 		})
 	})
-	const directoryAudio = path.join(__dirname, "Audios")
-	fs.readdir(directoryAudio, function (err, files){
-		if(err) return res.send("Could not scan directory.\n\n" + err)
+
+	fs.readdir("./Audios", function (err, files){
+		if(err) return res.write("Could not scan directory.\n\n" + err)
 		if(files.length === 0) return res.send("Directory Audios empty!")
 		console.log("\nAudios:\n")
 		files.forEach(function (file){
 			res.write(`${file}\n`)
+			console.log(`${file}\n`)
 		})
-		res.end()
+		//res.end()
 	})
 })
+
+app.get("/stream", async (req, res) => {
+	if(!req.query.id) return res.send("You did not provide a video id!")
+	let getVideo = await fs.statSync(`./Videos/${req.query.id}.mp4`)
+	if(!getVideo) return res.send("This Video ID does not exist!")
+	res.render("streamVideo", { videoName: `/video?id=${req.query.id}` })
+})
+
+app.get("/video", function (req, res) {
+	// Ensure there is a range given for the video
+	const range = req.headers.range;
+	if (!range) {
+		res.status(400).send("Requires Range header");
+	}
+
+	const videoPath = `./Videos/${req.query.id}.mp4`;
+	const videoSize = fs.statSync(`./Videos/${req.query.id}.mp4`).size;
+	// Parse Range
+	//Example: "bytes=32324-"
+	const CHUNK_SIZE = 10 ** 6; // 1MB
+	const start = Number(range.replace(/\D/g, ""));
+	const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+	// Create headers
+	const contentLength = end - start + 1;
+	const headers = {
+		"Content-Range": `bytes ${start}-${end}/${videoSize}`,
+		"Accept-Ranges": "bytes",
+		"Content-Length": contentLength,
+		"Content-Type": "video/mp4",
+	};
+
+	// HTTP Status 206 for Partial Content
+	res.writeHead(206, headers);
+
+	// create video read stream for this particular chunk
+	const videoStream = fs.createReadStream(videoPath, { start, end });
+
+	// Stream the video chunk to the client
+	videoStream.pipe(res);
+});
 
 app.use(function(req,res){
     res.status(404).send("🍌, 404")
 });
 app.listen(port, () => {
-  console.log(`Express is running on port ${port}`);
+  console.log(`http://${config.dns}:${port}`);
 })
